8508b2879debd0c40bd6ab9c4c50efcf47189a66
asirimece <asirimece@gmail.com>
2023-10-04 14:20:22 +0200

    credentials

--- speakeasy-python-client-library/speakeasypy/__init__.py
+++ speakeasy-python-client-library/speakeasypy/__init__.py
@@ -2,7 +2,7 @@ from speakeasypy.src.speakeasy import Speakeasy
 from speakeasypy.src.chatroom import Chatroom
 
 
-speakeasy = Speakeasy(host='https://speakeasy.ifi.uzh.ch', username='ecezeynep.asirim', password='qcje-6hLzZcNkA')
+speakeasy = Speakeasy(host='https://speakeasy.ifi.uzh.ch', username='kindle-pizzicato-wheat_bot', password='zJD7llj0A010Zg')
 speakeasy.login()
 
 # Only check active chatrooms (i.e., remaining_time > 0) if active=True.
