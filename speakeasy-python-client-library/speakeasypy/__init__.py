fb2adadeebcb34d8f55a8be94e93b3f34843d64b
masera <alberto.masera@uzh.ch>
2023-10-04 14:11:06 +0200

    working version

--- speakeasy-python-client-library/speakeasypy/__init__.py
+++ speakeasy-python-client-library/speakeasypy/__init__.py
@@ -1,2 +1,9 @@
 from speakeasypy.src.speakeasy import Speakeasy
 from speakeasypy.src.chatroom import Chatroom
+
+
+speakeasy = Speakeasy(host='https://speakeasy.ifi.uzh.ch', username='ecezeynep.asirim', password='qcje-6hLzZcNkA')
+speakeasy.login()
+
+# Only check active chatrooms (i.e., remaining_time > 0) if active=True.
+rooms = speakeasy.get_rooms(active=True)
